b8f5379cd0eee4dde6ff32debf72bec037e8571f
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2022-04-27 22:10:14 +0200

    added middleware

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import store from "../js/store/index";
-import { addArticle } from "../js/actions/index";
+import { addArticle } from "./actions";
 
 window.store = store;
 window.addArticle = addArticle;
