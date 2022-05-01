62fa076a51f361dcf22040c0312cd52dd3e08596
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2022-05-01 19:30:21 +0200

    added redux-thunk and middlewaeres for it

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,5 +1,24 @@
-import {ADD_ARTICLE} from "../constants/action-types";
+import { ADD_ARTICLE, DATA_LOADED } from '../constants/action-types';
 
 export function addArticle(payload) {
-  return {type: ADD_ARTICLE, payload}
+	return { type: ADD_ARTICLE, payload };
+}
+
+// our new action creator. Will it work? it will not work
+export function getDataWithoutThunk() {
+	return fetch('https://jsonplaceholder.typicode.com/posts')
+		.then(response => response.json())
+		.then(json => {
+			return { type: 'DATA_LOADED', payload: json };
+		});
+}
+// this will work, as this is redux-thunk
+export function getData() {
+	return function (dispatch) {
+		return fetch('https://jsonplaceholder.typicode.com/posts')
+			.then(response => response.json())
+			.then(json => {
+				dispatch({ type: DATA_LOADED, payload: json });
+			});
+	};
 }
